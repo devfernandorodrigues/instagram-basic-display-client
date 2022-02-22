c6730c27ba58b8160de846477af7090410bf7a8c
Fernando Rodrigues <devfernandorodrigues@gmail.com>
2022-02-21 22:35:40 -0300

    refactor: excute refresh with request method

--- client.py
+++ client.py
@@ -74,6 +74,6 @@ class InstagramBasicDisplayClient:
         }
 
         url = f"{self.GRAPH_ENDPOINT}/refresh_access_token"
-        resp = requests.get(url, params=params)
+        resp = self.request("get", url, params=params)
 
         return Authentication(**resp.json())
