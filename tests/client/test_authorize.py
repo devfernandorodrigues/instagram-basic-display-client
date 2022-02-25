2cf7e8b1ba6b2509804217d0ed7eba76d7abba42
Fernando Rodrigues <devfernandorodrigues@gmail.com>
2022-02-25 14:46:46 -0300

    refactor: test

--- tests/client/test_authorize.py
+++ tests/client/test_authorize.py
@@ -2,26 +2,17 @@ import urllib
 
 from faker import Faker
 
-from instabd.client import InstabdClient
-
 fake = Faker()
 
 AUTHORIZE_URL = "https://api.instagram.com/oauth/authorize"
 
 
-def test_url():
-    client_id = fake.uuid4()
-    client_secret = fake.uuid4()
-    redirect_uri = fake.url()
+def test_url(client):
     scope = "user_profile,user_media"
-    client = InstabdClient(
-        client_id=client_id,
-        client_secret=client_secret,
-        redirect_uri=redirect_uri,
-    )
+    client.scope = scope
     params = {
-        "client_id": client_id,
-        "redirect_uri": redirect_uri,
+        "client_id": client.client_id,
+        "redirect_uri": client.redirect_uri,
         "scope": scope,
         "response_type": "code",
     }
@@ -32,20 +23,12 @@ def test_url():
     assert url == expected_url
 
 
-def test_scope():
-    client_id = fake.uuid4()
-    client_secret = fake.uuid4()
-    redirect_uri = fake.url()
+def test_scope(client):
     scope = "user_profile"
-    client = InstabdClient(
-        client_id=client_id,
-        client_secret=client_secret,
-        redirect_uri=redirect_uri,
-        scope=scope,
-    )
+    client.scope = scope
     params = {
-        "client_id": client_id,
-        "redirect_uri": redirect_uri,
+        "client_id": client.client_id,
+        "redirect_uri": client.redirect_uri,
         "scope": scope,
         "response_type": "code",
     }
